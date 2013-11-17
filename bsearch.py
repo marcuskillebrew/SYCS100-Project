20ce75b0cf45802ccb4baf7fce67b6736aec18e3
marcus.killebrew <marcus.killebrew@bison.howard.edu>
2013-11-17 14:28:43 -0500

    Update bsearch.py

--- bsearch.py
+++ bsearch.py
@@ -2,7 +2,7 @@ SYCS100-Project
 ===============
 def binarySearch(BostonCeltics,MiamiHeat):
     found = False
-    low = 0
+    low = 14
     high = len(MiamiHeat)-1
     while low<=high and not found:
         middle = (low+high)//2
@@ -16,13 +16,16 @@ def binarySearch(BostonCeltics,MiamiHeat):
 
 MiamiHeat= [15,24,27,30,35,45,50,55]    
 BostonCeltics= [15,20,26,32,35,45,50,80]
+points = [15,24,27,30,35,45,50,55]
 if MiamiHeat>=5:
     MiamiHeat = [15,24,27,30,33,35,45,50,55]
     team = int(input("How many points did your team score?"))
     isitFound= binarySearch(BostonCeltics,MiamiHeat)
-    if isitFound is True:
-        print("is in index")
+    if isitFound: 
+        print("points")
     else:
         print("-1")
+    
+
 #My Binary Search that uses NBA Teams
 #This BinarySearch cuts the list in half by 2 when searching.
